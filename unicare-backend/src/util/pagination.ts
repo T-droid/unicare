771c0dae892e4a6a910891c4d492b26007725236
= <=>
2025-02-20 11:30:37 +0300

    refactor(pagination): change default behaviour for pagination

--- unicare-backend/src/util/pagination.ts
+++ unicare-backend/src/util/pagination.ts
@@ -1,5 +1,5 @@
 export const getPaginationParams = (query: Record<string, any>) => {
-   // The Record is more like `dict` in python
+  // The Record is more like `dict` in python
   const page = Math.max(1, parseInt(query.page as string, 10) || 1);
   const limit = Math.max(1, parseInt(query.limit as string, 10) || 10);
   const offset = (page - 1) * limit;
