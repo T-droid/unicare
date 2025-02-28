17e2aa1c1aa63ed75f95783da10f9f4b7bb0e4f1
kc-allan <allancheruiyot003@gmail.com>
2025-02-28 13:31:57 +0300

    cors

--- unicare-backend/src/app.ts
+++ unicare-backend/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: "*", // allow all origins
+    origin: "http://localhost:5173", // allow all origins
     credentials: true, // allow cookies to b sent
   }
 ));
