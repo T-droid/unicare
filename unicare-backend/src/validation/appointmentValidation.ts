cc87ee6ec37773a6fa680f2986fad8904ec8da74
kc-allan <allancheruiyot003@gmail.com>
2025-03-26 11:53:22 +0300

    Auto-lint changes

--- unicare-backend/src/validation/appointmentValidation.ts
+++ unicare-backend/src/validation/appointmentValidation.ts
@@ -3,8 +3,8 @@ import Joi from "joi";
 export const appointmentSchema = Joi.object({
   regNo: Joi.string().required(),
   doctorId: Joi.string()
-  .guid({ version: ["uuidv4", "uuidv5"] })
-  .required(),
+    .guid({ version: ["uuidv4", "uuidv5"] })
+    .required(),
   date: Joi.string()
     .isoDate()
     .custom((value, helpers) => {
